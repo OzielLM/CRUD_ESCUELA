e09849ca2f47996c8102fe1ac17a6b5330407c83
Arnulfo <josearnulfogaytanalvarez@gmail.com>
2024-12-05 14:34:29 -0600

    materiasCM.js completado

---
+++ screens/Js/materiasCM.js
@@ -0,0 +1,31 @@
+const formularioMateria = document.getElementById('formularioMateria');
+let materias = JSON.parse(localStorage.getItem('materias')) || [];
+let editIndex = -1;
+const index = localStorage.getItem('index');
+
+formularioMateria.addEventListener('submit', function(event) {
+    console.log(formularioMateria);
+    event.preventDefault();
+    const nombre = document.getElementById('nombreMateriaCal').value;
+
+    if(index!=null){
+        materias[index]={ nombre };
+        localStorage.setItem("materias", JSON.stringify(materias));
+        window.location.href='materiasView.html';
+    } else {
+        materias.push({ nombre });
+        localStorage.setItem("materias", JSON.stringify(materias));
+        window.location.href='materiasView.html';
+    }
+});
+
+if(index!=null){
+    const materia = materias[index];
+    document.getElementById('nombreMateriaCal').value = materia.nombre;
+}
+
+function editarMateria(index) {
+    editIndex = index;
+    const materia = materias[index];
+    document.getElementById('nombreMateriaCal').value = materia.nombre;
+}
